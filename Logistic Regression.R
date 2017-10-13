337257ebc5f29f70ea5b519e8ec142bc6ddc0b45
patrick90x <plyons@utexas.edu>
2017-10-12 20:26:37 -0500

    Made one edit to reflect the correct commenting

--- Logistic Regression.R
+++ Logistic Regression.R
@@ -28,7 +28,7 @@ glm.pred=ifelse(glm.probs>0.5,1,0)
 #will create vector of trues and falses; for each element, if it's greater than .5 we call it heart disease
 attach(df) #allows us to use variables by name
 table(glm.pred,num) #allows us to compare our predictions to the actual data
-mean(glm.pred==num) #classification performance: gives us proportion, in this case .5216
+mean(glm.pred==num) #classification performance: gives us proportion
 
 plot(oldpeak,num,xlab="Oldpeak",ylab="Probability of Heart Disease")
 g=glm(num~oldpeak,data=df,family=binomial)
